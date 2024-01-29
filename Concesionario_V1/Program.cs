df62267302c836769541a804ed552bd423bb15f4
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-01-29 12:04:11 +0100

    Agregar archivos de proyecto.

---
+++ Concesionario_V1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Concesionario_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            // MENÚ DE ACCIONES 
+
+            // PROCESO DE LAS ACCIONES
+
+
+
+        }
+    }
+}
